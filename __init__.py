4a36d9a721fe8298e5b44cdfc12c9df991e40243
Sophie Laturnus <SLaturnus@googlemail.com>
2017-07-20 17:40:02 +0200

    BUG: import of psignifit did not reveal functions.

--- __init__.py
+++ __init__.py
@@ -1,6 +1,15 @@
 # -*- coding: utf-8 -*-
+#! 
 """
 Created on Sat Dec  5 20:49:14 2015
 
 @author: root
 """
+
+from .psignifit import *
+
+__title__ = 'psignifit'
+__version__ = '0.1'
+__author__ = 'Sophie Laturnus <sophie.laturnus@uni-tuebingen.de> www.wichmann-lab.org'
+__notes__ = 'based on psignifit by Heiko Schuett with help from Felix Wichmann,  Jakob Macke and Stefan Harmeling'
+__license__ = 'GPL, see LICENSE'
