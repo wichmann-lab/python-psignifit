3258d2d0abba6b4648f323483e579b7c0164c6c2
Tiziano Zito <opossumnano@gmail.com>
2017-10-25 20:52:50 +0200

    fix git revision parsing

--- psignifit/__init__.py
+++ psignifit/__init__.py
@@ -5,7 +5,6 @@ Felix Wichmann, Jakob Macke and Stefan Harmeling.
 """
 import os
 import subprocess
-from .psignifit import *
 
 __name__ =        'psignifit'
 __description__ = 'toolbox for Bayesian psychometric function estimation'
@@ -15,11 +14,18 @@ __author__ =      'Sophie Laturnus <sophie.laturnus@uni-tuebingen.de> www.wichma
 __license__ =     'GPLv3+'
 __revision__ =    'N/A'
 
-# get the HEAD sha from git if we are in a git repo (only useful for devs)
-try:
-    CWD = os.path.abspath(os.path.dirname(__file__))
-    output = subprocess.check_output(['git', 'describe', '--tags', '--dirty=+'],
-                                     cwd=CWD, stderr=subprocess.PIPE)
-    __revision__ = output.strip()
-except Exception:
-    pass
+# get the git SHA if we are in a git repo (only useful for devs)
+
+# current dir
+CWD = os.path.abspath(os.path.dirname(__file__))
+# try two options for getting the git revision
+# - nice version with tags
+# - plain SHA
+for cmd in ('git describe --tags --dirty=+'), ('git rev-parse HEAD'):
+    try:
+        proc = subprocess.check_output(cmd.split(), cwd=CWD,
+                stderr=subprocess.PIPE, universal_newlines=True)
+        __revision__ = proc.strip()
+    except Exception:
+        # ok, don't bother
+        pass
