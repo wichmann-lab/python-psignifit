5a794ed357d457b855a238b06dcf05bec8d41ff6
Tiziano Zito <opossumnano@gmail.com>
2017-10-25 19:20:37 +0200

    update __init__.py with proper metadata and git revision

--- psignifit/__init__.py
+++ psignifit/__init__.py
@@ -1,15 +1,25 @@
 # -*- coding: utf-8 -*-
-#! 
 """
-Created on Sat Dec  5 20:49:14 2015
-
-@author: root
+Python psignifit - based on Matlab psignifit by Heiko Schütt with help from Felix Wichmann,
+Jakob Macke and Stefan Harmeling.
 """
-
+import os
+import subprocess
 from .psignifit import *
 
-__title__ = 'psignifit'
-__version__ = '0.1'
-__author__ = 'Sophie Laturnus <sophie.laturnus@uni-tuebingen.de> www.wichmann-lab.org'
-__notes__ = 'based on psignifit by Heiko Schuett with help from Felix Wichmann,  Jakob Macke and Stefan Harmeling'
-__license__ = 'GPL, see LICENSE'
+__name__ =        'psignifit'
+__description__ = 'toolbox for Bayesian psychometric function estimation'
+__version__ =     '0.1'
+__url__ =         'https://github.com/wichmann-lab/python-psignifit'
+__author__ =      'Sophie Laturnus <sophie.laturnus@uni-tuebingen.de> www.wichmann-lab.org'
+__license__ =     'GPLv3+'
+__revision__ =    'N/A'
+
+# get the HEAD sha from git if we are in a git repo (only useful for devs)
+try:
+    CWD = os.path.abspath(os.path.dirname(__file__))
+    output = subprocess.check_output(['git', 'describe', '--tags', '--dirty=+'],
+                                     cwd=CWD, stderr=subprocess.PIPE)
+    __revision__ = output.strip()
+except Exception:
+    pass
