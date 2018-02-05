2f9884ab5c8175acc76fd1935888ce7b9d873e94
Tiziano Zito <opossumnano@gmail.com>
2018-02-05 12:08:17 +0100

    use namespaces

--- psignifit/utils.py
+++ psignifit/utils.py
@@ -2,26 +2,25 @@
 """
 Utils class capsulating all custom made probabilistic functions
 """
-
-from numpy import ndarray, array, log, log1p, exp, sqrt, tan, pi, nan, isnan, inf, zeros, shape, ravel, tile, logical_and, logical_or
-from scipy.special import betainc,betaln,erfcinv, erfc
+import numpy as np
+import scipy.special as ss
 
 def my_norminv(p,mu,sigma):
-    
-    x0 = -sqrt(2)*erfcinv(2*p)
+
+    x0 = -np.sqrt(2)*ss.erfcinv(2*p)
     x = sigma*x0 + mu
-    
+
     return x
 
 def my_normcdf(x,mu,sigma):
     z = (x-mu) /sigma
-    p = .5*erfc(-z/sqrt(2))
-    
+    p = .5*ss.erfc(-z/np.sqrt(2))
+
     return p
 
-    
+
 def my_t1cdf(x):
-    '''    
+    '''
     cumulative distribution function of a t-dist. with 1 degree of freedom
     function p=my_t1cdf(x)
     input
@@ -32,43 +31,43 @@ def my_t1cdf(x):
     see also: tcdf 
     '''
     xsq=x*x;
-    p = betainc(1/2, 1/2,1 / (1 + xsq)) / 2
+    p = ss.betainc(1/2, 1/2,1 / (1 + xsq)) / 2
     p[x>0]=1-p[x>0]
-    
-    return p    
+
+    return p
 
 def my_t1icdf(p):
-    x = tan(pi * (p - 0.5));
-    return x    
+    x = np.tan(np.pi * (p - 0.5));
+    return x
 
 def my_betapdf(x,a,b):
     ''' this implements the betapdf with less input checks '''
-    
+
     if type(x) is int or float:
-        x = array(x)
+        x = np.array(x)
 
     # Initialize y to zero.
-    y = zeros(shape(x))
+    y = np.zeros_like(x)
 
-    if len(ravel(a)) == 1:
-        a = tile(a,shape(x))
+    if len(np.ravel(a)) == 1:
+        a = np.tile(a, x.shape())
 
-    if len(ravel(b)) == 1:
-        b = tile(b,shape(x))
+    if len(np.ravel(b)) == 1:
+        b = np.tile(b, x.shape())
 
     # Special cases
-    y[logical_and(a==1, x==0)] = b[logical_and(a==1 , x==0)]
-    y[logical_and(b==1 , x==1)] = a[logical_and(b==1 , x==1)]
-    y[logical_and(a<1 , x==0)] = inf
-    y[logical_and(b<1 , x==1)] = inf
+    y[np.logical_and(a==1, x==0)] = b[np.logical_and(a==1 , x==0)]
+    y[np.logical_and(b==1 , x==1)] = a[np.logical_and(b==1 , x==1)]
+    y[np.logical_and(a<1 , x==0)] = np.inf
+    y[np.logical_and(b<1 , x==1)] = np.inf
 
     # Return NaN for out of range parameters.
-    y[a<=0] = nan
-    y[b<=0] = nan
-    y[logical_or(logical_or(isnan(a), isnan(b)), isnan(x))] = nan
+    y[a<=0] = np.nan
+    y[b<=0] = np.nan
+    y[np.logical_or(np.logical_or(np.isnan(a), np.isnan(b)), np.isnan(x))] = np.nan
 
     # Normal values
-    k = logical_and(logical_and(a>0, b>0),logical_and(x>0 , x<1))
+    k = np.logical_and(np.logical_and(a>0, b>0),np.logical_and(x>0 , x<1))
     a = a[k]
     b = b[k]
     x = x[k]
@@ -76,51 +75,51 @@ def my_betapdf(x,a,b):
     # Compute logs
     smallx = x<0.1
 
-    loga = (a-1)*log(x)
+    loga = (a-1)*np.log(x)
 
-    logb = zeros(shape(x))
-    logb[smallx] = (b[smallx]-1) * log1p(-x[smallx])
-    logb[~smallx] = (b[~smallx]-1) * log(1-x[~smallx])
+    logb = np.zeros(x.shape())
+    logb[smallx] = (b[smallx]-1) * np.log1p(-x[smallx])
+    logb[~smallx] = (b[~smallx]-1) * np.log(1-x[~smallx])
 
-    y[k] = exp(loga+logb - betaln(a,b))
+    y[k] = np.exp(loga+logb - ss.betaln(a,b))
 
     return y
-    
+
 def fill_kwargs(kw_args, values):
     '''
     Fill the empty dictionary kw_args with the values given in values.
     values are assigned in the order alpha, beta, lambda, gamma, varscale.
     '''
-    
+
     kw_args.clear()
-    d = len(values)    
+    d = len(values)
     for i in range(0,d):
         if i == 0:
-            if type(values[0]) == ndarray:
+            if type(values[0]) == np.ndarray:
                 kw_args['alpha'] = values[0]
             else:
-                kw_args['alpha'] = array([values[0]])
+                kw_args['alpha'] = np.array([values[0]])
         if i == 1:
-            if type(values[1]) == ndarray:
+            if type(values[1]) == np.ndarray:
                 kw_args['beta'] = values[1]
             else:
-                kw_args['beta'] = array([values[1]])                
+                kw_args['beta'] = np.array([values[1]])
         if i == 2:
-            if type(values[2]) == ndarray:
+            if type(values[2]) == np.ndarray:
                 kw_args['lambda'] = values[2]
             else:
-                kw_args['lambda'] = array([values[2]])
+                kw_args['lambda'] = np.array([values[2]])
         if i == 3:
-            if type(values[3]) == ndarray:
+            if type(values[3]) == np.ndarray:
                 kw_args['gamma'] = values[3]
             else:
-                kw_args['gamma'] = array([values[3]])
+                kw_args['gamma'] = np.array([values[3]])
         if i == 4:
-            if type(values[4]) == ndarray:
+            if type(values[4]) == np.ndarray:
                 kw_args['varscale'] = values[4]
             else:
-                kw_args['varscale'] = array([values[4]])
-        
+                kw_args['varscale'] = np.array([values[4]])
+
 def strToDim(string):
     """
     Finds the number corresponding to a dim/parameter given as a string. 
@@ -133,4 +132,4 @@ def strToDim(string):
     elif s in ['gamma','guess','guessrate','guess rate',
                'guess-rate','lower asymptote','g', '3']:    return 3, r'$\gamma$'
     elif s in ['sigma','std','s','eta','e', '4']:           return 4, r'$\eta$'
-    
+
