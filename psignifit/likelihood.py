8f9f99a44c682244b20cb8678af240c5d4d1a5f4
Tiziano Zito <opossumnano@gmail.com>
2018-02-21 13:46:33 +0100

    trailing whitespace fixes

--- psignifit/likelihood.py
+++ psignifit/likelihood.py
@@ -98,18 +98,18 @@ def likelihood(data, options, args):
     very small and this way stay in the range representable in floats
 
     """
-    
+
     p = logLikelihood(data, options, args)
-        
+
     '''We never need the actual value of the likelihood. Something proportional
     is enough and this circumvents numerical problems for the likelihood to
     become exactly 0'''
-    
-    logPmax = np.max(p)    
-    
+
+    logPmax = np.max(p)
+
     p = p -np.max(p)
     p = np.exp(p)
-    
+
     return (p,logPmax)
 
 
@@ -122,10 +122,10 @@ def logLikelihood(data,options, args):
     psignifit.
     this actually adds the log priors as well. Technically it calculates the
     unnormalized log-posterior
-    
+
     """
 
-    
+
     sigmoidHandle = getattr(sigmoids, options['sigmoidName'])
     # curry sigmoid function to fix alpha and threshPC
     sigmoidHandle = functools.partial(sigmoidHandle, PC=options['threshPC'], alpha=options['widthalpha'])
@@ -138,10 +138,10 @@ def logLikelihood(data,options, args):
         else:
             alpha = np.array([args[0]])
         if hasattr(args[1], '__iter__'):
-            beta = args[1]        
+            beta = args[1]
         else:
             beta = np.array([args[1]])
-            
+
         if len(args) > 2:
             if hasattr(args[2], '__iter__'):
                 lamb = args[2]
@@ -163,11 +163,11 @@ def logLikelihood(data,options, args):
                 varscale = np.array([args[4]])
         else:
             varscale = np.array([1])
-    
+
     # is the input only one point?
-    oneParameter = not(len(alpha) > 1 or len(beta) > 1 or len(lamb) > 1 
+    oneParameter = not(len(alpha) > 1 or len(beta) > 1 or len(lamb) > 1
                 or len(gamma) > 1 or len(varscale) > 1)
-    
+
     if oneParameter:     # in optimization if the parameter supplied is not the fixed value
         if np.isfinite(np.array(options['fixedPars'][0])):
             alpha = options['fixedPars'][0]
@@ -185,15 +185,15 @@ def logLikelihood(data,options, args):
             gamma = np.nan
         if (varscale<0) or (varscale>1):
             varscale = np.nan
-    else:        
+    else:
         #issues for automization: limit range for lambda & gamma
         lamb[np.logical_or(lamb < 0 ,lamb > (1-np.max(gamma)))] = np.nan
         gamma[np.logical_or(gamma < 0, gamma > (1-np.max(lamb)))] = np.nan
         varscale[np.logical_or(varscale < 0, varscale > 1)] = np.nan
-    
+
     varscaleOrig = np.reshape(varscale, [1,1,1,1,-1]);
 
-    
+
     if oneParameter:
         if options['expType'] == 'equalAsymptote':
             gamma = lamb
@@ -205,7 +205,7 @@ def logLikelihood(data,options, args):
         n = np.array(data[:,2])
         k = np.array(data[:,1])
         varscale = varscale**2;
-        
+
         if varscale < 10**-9:
             p = p + k * np.log(psi) + (n-k)*np.log(1-psi)   # binomial model
         else:
@@ -219,7 +219,7 @@ def logLikelihood(data,options, args):
         if np.isnan(p):
             p = - np.inf
     else:       # for grid evaluation
-        
+
         alpha = np.reshape(alpha, [-1,1,1,1,1])
         beta = np.reshape(beta, [1, -1, 1, 1,1])
         lamb = np.reshape(lamb, [1,1,-1, 1, 1])
@@ -227,7 +227,7 @@ def logLikelihood(data,options, args):
         varscale = np.reshape(varscale,[1,1,1,1,-1])
         varscale = varscale**2          # go from sd to variance
         vbinom = (varscale < 10**-9)    # for very small variance use the binomial model
-        
+
         v = varscale[~vbinom]
         v = 1/v -1
         v = np.reshape(v, [1,1,1,1, -1])
@@ -235,26 +235,26 @@ def logLikelihood(data,options, args):
         pbin = np.zeros((1,1,1,1,1))    # posterior for binomial work
         n = np.size(data,0)
         levels = np.array(data[:,0])    # needed for GPU work
-    
-        
+
+
         if options['expType'] == 'equalAsymptote':
             gamma = lamb
-        
+
         scale = 1-gamma-lamb
         for i in range(0,n):
-            if options['verbose'] > 3: 
+            if options['verbose'] > 3:
                 print('\r%d/%d', i,n)
             xi = levels[i]
-            psi = sigmoidHandle(xi,alpha,beta) 
+            psi = sigmoidHandle(xi,alpha,beta)
             psi = psi*scale + gamma
             ni = np.array(data[i,2])
             ki = np.array(data[i,1])
-            
+
             if ((ni-ki)>0 and ki > 0):
                 psi[psi < 1E-200] = 1E-200
                 temp = 1-psi
                 temp[temp < 1E-200] = 1E-200
-                
+
                 pbin = pbin + ki * np.log(psi) + (ni-ki)*np.log(temp)
                 if (v.size != 0):
                     a = psi * v
@@ -267,7 +267,7 @@ def logLikelihood(data,options, args):
             elif ki > 0:    # --> ni-ki == 0
                 psi[psi < 1E-200] = 1E-200
                 pbin  = pbin + ki * np.log(psi);
-                if (v.size != 0):                                             
+                if (v.size != 0):
                     a = psi*v
                     p = p + sp.gammaln(ki + a)
                     p = p - sp.gammaln(ni+v)
@@ -275,7 +275,7 @@ def logLikelihood(data,options, args):
                     p = p + sp.gammaln(v)
                 else:
                     p = np.array([])
-            
+
             elif (ni-ki) > 0 :  # --> ki ==0
                 pbin = pbin  + (ni-ki)*np.log(1-psi)
                 if (v.size != 0):
@@ -285,7 +285,7 @@ def logLikelihood(data,options, args):
                     p = p + sp.gammaln(v)
                 else:
                     p = np.array([])
-        
+
         if options['verbose'] > 3 :
             print('\n')
         if (p.size == 0):
@@ -293,9 +293,9 @@ def logLikelihood(data,options, args):
         else:
             p = np.concatenate((np.tile(pbin, [1,1,1,1,np.sum(vbinom)]),p), axis=4)
         p[np.isnan(p)] = -np.inf
-      
+
     if (options['priors']):
-        
+
         if isinstance(options['priors'], list):
             if hasattr(options['priors'][0], '__call__'):
                 temp = options['priors'][0](alpha)
@@ -306,34 +306,34 @@ def logLikelihood(data,options, args):
                 p += prior
             if hasattr(options['priors'][1], '__call__'):
                 temp = options['priors'][1](beta)
-                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200         
-                
+                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200
+
                 prior = np.log(temp)
                 if not(oneParameter):
                     prior = np.tile(prior, (p.shape[0],1) +p.shape[2:])
                 p += prior
             if hasattr(options['priors'][2], '__call__'):
                 temp = options['priors'][2](lamb)
-                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200             
-                
+                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200
+
                 prior = np.log(temp)
                 if not(oneParameter):
                     prior = np.tile(prior,p.shape[0:2] + (1,) + p.shape[3:])
                 p += prior
             if hasattr(options['priors'][3], '__call__'):
                 temp = options['priors'][3](gamma)
-                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200   
-                prior = np.log(temp) 
+                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200
+                prior = np.log(temp)
                 if not(oneParameter):
                     if options['expType'] == 'equalAsymptote':
-                        prior = np.tile(prior, p.shape[0:2] +(1,1,p.shape[4])) 
+                        prior = np.tile(prior, p.shape[0:2] +(1,1,p.shape[4]))
                     else:
-                        prior = np.tile(prior, p.shape[0:3] +(1,p.shape[4])) 
+                        prior = np.tile(prior, p.shape[0:3] +(1,p.shape[4]))
                 p += prior
             if hasattr(options['priors'][4], '__call__'):
                 temp = options['priors'][4](varscaleOrig)
-                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200                
-                
+                if len(np.ravel(temp)) > 1: temp[temp < 1E-200] = 1E-200
+
                 prior = np.log(temp)
                 if not(oneParameter):
                     prior = np.tile(prior, p.shape[0:-1]+(1,))
@@ -341,12 +341,12 @@ def logLikelihood(data,options, args):
                     prior = np.squeeze(prior)
                 p += prior
 
-    return p  
+    return p
 
-        
+
 if __name__ == "__main__":
     import sys
     likelihood(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6], sys.argv[7])
 
-    
-        
+
+
