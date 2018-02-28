fe7756b4cc6f944a8f6f7db9afd0a9261f9ef9a0
Tiziano Zito <opossumnano@gmail.com>
2018-02-28 10:44:37 +0100

    remove old cruft from borders.py

--- psignifit/borders.py
+++ psignifit/borders.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import numpy as np
 
-from .getWeights import getWeights
 from .likelihood import likelihood
 from .marginalize import marginalize
 from .utils import norminv
@@ -52,50 +51,4 @@ def set_borders(data, wmin=None, etype=None, srange=None,
 
     return {'threshold': threshold, 'width': width, 'lambda': lambda_,
             'gamma': gamma, 'eta' : eta}
-
-def moveBorders(data,options):
-    """
-    move parameter-boundaries to save computing power
-    function borders=moveBorders(data, options)
-    this function evaluates the likelihood on a much sparser, equally spaced
-    grid definded by mbStepN and moves the borders in so that that
-    marginals below tol are taken away from the borders.
-
-    this is meant to save computing power by not evaluating the likelihood in
-    areas where it is practically 0 everywhere.
-    """
-    borders = []
-
-    tol = options['maxBorderValue']
-    d = options['borders'].shape[0]
-
-    MBresult = {'X1D':[]}
-
-    ''' move borders inwards '''
-    for idx in range(0,d):
-        if (len(options['mbStepN']) >= idx and options['mbStepN'][idx] >= 2
-            and options['borders'][idx,0] != options['borders'][idx,1]) :
-            MBresult['X1D'].append(np.linspace(options['borders'][idx,0], options['borders'][idx,1], options['mbStepN'][idx]))
-        else:
-            if (options['borders'][idx,0] != options['borders'][idx,1] and options['expType'] != 'equalAsymptote'):
-                warnings.warn('MoveBorders: You set only one evaluation for moving the borders!')
-
-            MBresult['X1D'].append( np.array([0.5*np.sum(options['borders'][idx])]))
-
-
-    MBresult['weight'] = getWeights(MBresult['X1D'])
-    MBresult['Posterior'] = likelihood(data, options, MBresult['X1D'])[0]
-    integral = sum(np.reshape(MBresult['Posterior'], -1) * np.reshape(MBresult['weight'], -1))
-    MBresult['Posterior'] /= integral
-
-    borders = np.zeros([d,2])
-
-    for idx in range(0,d):
-        (L1D,x,w) = marginalize(MBresult, np.array([idx]))
-        x1 = x[np.max([np.where(L1D*w >= tol)[0][0] - 1, 0])]
-        x2 = x[np.min([np.where(L1D*w >= tol)[0][-1]+1, len(x)-1])]
-
-        borders[idx,:] = [x1,x2]
-
-    return borders
 
