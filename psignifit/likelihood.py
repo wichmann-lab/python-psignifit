5d808ee8671920dfaf527d2e2117ab7671e46308
Tiziano Zito <opossumnano@gmail.com>
2018-07-13 11:21:31 +0200

    reworked likelihood and added a curried version for optimization

--- psignifit/likelihood.py
+++ psignifit/likelihood.py
@@ -4,7 +4,19 @@ import numpy as np
 import scipy.special as sp
 
 from . import sigmoids
-from .utils import fp_error_handler
+from .utils import fp_error_handler, PsignifitException
+
+# this is the order in which the parameters are stored in the big
+# likelihood 5 dimensional matrix
+PARM_ORDER = ('threshold', 'width', 'lambda', 'gamma', 'eta')
+
+def likelihood(data, sigmoid=None, priors=None, grid=None):
+    p = log_likelihood(data, sigmoid=sigmoid, priors=priors, grid=grid)
+    logmax = np.max(p)
+    p -= logmax
+    p = np.exp(p)
+    return p, logmax
+
 
 @fp_error_handler(divide='ignore')
 def log_likelihood(data, sigmoid=None, priors=None, grid=None):
@@ -13,15 +25,24 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
     width = grid['width']
     lambd = grid['lambda']
     gamma = grid['gamma']
-    parm_order = ('threshold', 'width', 'lambda', 'gamma')
 
+    levels = data[:,0]
+    ncorrect = data[:,1]
+    ntrials = data[:,2]
+
+    ###FIXME: fix treatment of no 'eta' configurations,
+    # best choice is probably having grid['eta'] = [0]
+    # then rewrite below so that we don't test for v -> None but for
+    # v -> [] instead, then rearrange the p, pbin broadcasting and
+    # concatenating?
+    # fixing this would also fix the case with the curried likelihood
+    # below, that currently does work for grid['eta'] -> None
     if grid['eta'] is None:
         v = None
         thres, width, lambd, gamma = np.meshgrid(thres, width, lambd, gamma,
                                                  copy=False, sparse=True,
                                                  indexing='ij')
     else:
-        parm_order = parm_order + ('eta', )
         eta_std = grid['eta']
         eta = eta_std**2 # use variance instead of standard deviation
         v = 1/eta[eta > 1e-09] - 1
@@ -32,47 +53,58 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
                                                     v, copy=False, sparse=True,
                                                     indexing='ij')
 
-    levels = data[:,0]
-    ncorrect = data[:,1]
-    ntrials = data[:,2]
 
     scale = 1 - gamma - lambd
     ###FIXME: handle the case with equal_asymptote
 
     pbin = 0
     p = 0
+    # we could get rid of the loop by playing crazy fancy-indexing tricks
+    # with the arrays here, but that version would be quite more unreadable.
+    # as we don't expect huge number of levels, we should not lose too much
+    # performance by using an explicit, but no profiling has been done to prove
+    # this.
     for i in range(len(levels)):
         x = levels[i]
-        # average predicted probability of correct
-        psi = sigmoid(x, thres, width)*scale + gamma
         n = ntrials[i]
         k = ncorrect[i]
+        if n == 0:
+            # no trials at this stimulus level!
+            continue
+        # average predicted probability of correct
+        psi = sigmoid(x, thres, width)*scale + gamma
         if k == 0:
-            pbin += pbin + n * np.log(1-psi)
+            # no correct
+            pbin += n * np.log(1-psi)
             if v is not None:
                 b = (1-psi)*v
                 p += (sp.gammaln(n+b) - sp.gammaln(n+v) - sp.gammaln(b) +
                       sp.gammaln(v))
         elif k == n:
+            # all correct
             pbin += k * np.log(psi)
             if v is not None:
                 a = psi*v
                 p += (sp.gammaln(k+a) - sp.gammaln(n+v) - sp.gammaln(a) +
                       sp.gammaln(v))
         elif k < n:
+            # some correct
             psi_r = 1-psi
             pbin += k*np.log(psi) + (n-k)*np.log(psi_r)
             if v is not None:
                 a = psi*v
                 b = (1-psi)*v
-                p += ( sp.gammaln(k+a) + sp.gammaln(n-k+b) - sp.gammaln(n+v) -
-                        sp.gammaln(a) - sp.gammaln(b) + sp.gammaln(v))
+                p += (sp.gammaln(k+a) + sp.gammaln(n-k+b) - sp.gammaln(n+v) -
+                      sp.gammaln(a) - sp.gammaln(b) + sp.gammaln(v))
         else:
-            # this is n==0, i.e. no trials done at this stimulus level
-            pass
+            # we should never land here: we can't more ncorrect than ntrials
+            raise PsignifitException('ncorrect %d > ntrials %d!'%(k, n))
 
-    pbin = np.broadcast_to(pbin, pbin.shape[:4]+(vbinom,))
-    p = np.concatenate((pbin, p), axis=4)
+    if v is None:
+        p = pbin
+    else:
+        pbin = np.broadcast_to(pbin, pbin.shape[:4]+(vbinom,))
+        p = np.concatenate((pbin, p), axis=4)
 
     # add priors on the corresponding axis (they get added on the right axis
     # because of the meshgrid
@@ -81,14 +113,29 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
     p += np.log(priors['lambda'](lambd))
     ## FIXME equal asymptote case not contemplated here
     p += np.log(priors['gamma'](gamma))
-    if grid['eta'] is not None:
+    if v is not None:
         p += np.log(priors['eta'](eta_std))
-    return p, parm_order
+    return p
 
-def likelihood(data, sigmoid=None, priors=None, grid=None):
-    p, p_order = log_likelihood(data, sigmoid=sigmoid, priors=priors, grid=grid)
-    logmax = np.max(p)
-    p -= logmax
-    p = np.exp(p)
-    return p, logmax, p_order
 
+def get_optm_llh(data, sigmoid=None, priors=None, grid=None):
+    ###FIXME### does not work when grid[parm] = None!!!
+    # return a curried version of log_likelihood to be used in the
+    # optimization routine
+    # - go through the grid and detect "fixed" parameters that we shouldn't
+    #   try to optimize
+    fixed_parms = [parm for parm, steps in grid.items() if len(steps) == 1]
+    # create a list of (fixed_parm_index, fixed_parm_value) tuples
+    fixed = [(PARM_ORDER.index(parm), grid[parm][0]) for parm in fixed_parms]
+
+    def cllh(x):
+        # convert to list for efficient insertion
+        y = list(x)
+        for fidx, fval in fixed:
+            # insert value of fixed parameter at the right position
+            y.insert(fidx, fval)
+        # create a temporary grid with only one step (all params are fixed)
+        lgrid = {parm:y[idx] for idx, parm in enumerate(PARM_ORDER)}
+        return -log_likelihood(data, sigmoid=sigmoid, priors=priors, grid=lgrid)
+
+    return cllh
