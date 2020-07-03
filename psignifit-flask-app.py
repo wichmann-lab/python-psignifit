2548e924fe4528571457fc656a90335ba74480b5
dorian <dorian@mino.rs>
2020-07-03 20:33:59 +0100

    demo js script for testing post request added

--- psignifit-flask-app.py
+++ psignifit-flask-app.py
@@ -1,49 +1,63 @@
-from flask import Flask, request
-import json
+from flask import Flask, request, render_template, jsonify
+#import json
 import numpy as np
 import psignifit as ps
 
 app = Flask(__name__)
 
-# then we need to process it
-@app.route("/")
-def fitting():
+@app.route('/')
+def output():
+    return render_template('demo.html')
 
-	# so we get data here
-	data = np.array([[0.0010,   45.0000,   90.0000],
-				 [0.15,   50.0000,   90.0000],
-				 [0.20,   44.0000,   90.0000],
-				 [0.25,   44.0000,   90.0000],
-				 [0.30,   52.0000,   90.0000],
-				 [0.35,   53.0000,   90.0000],
-				 [0.40,   62.0000,   90.0000],
-				 [0.45,   64.0000,   90.0000],
-				 [0.50,   76.0000,   90.0000],
-				 [0.60,   79.0000,   90.0000],
-				 [0.70,   88.0000,   90.0000],
-				 [0.80,   90.0000,   90.0000],
-				 [0.1,   90.0000,   90.0000]]);
+@app.route("/psignifit", methods=['POST'])
+def calculate(): 
+    
+    def reciever():
+        if request.is_json() is None: # some sanitising here - only allow JSON
+            abort(404)
+        else:
+            recieved_data = request.get_json()
 
-	options = dict();   # initialize as an empty dictionary
-	options['sigmoidName'] = 'norm';   # choose a cumulative Gauss as the sigmoid  
-	options['expType']     = '2AFC';   # choose 2-AFC as the experiment type  
-									   # this sets the guessing rate to .5 (fixed) and  
-									   # fits the rest of the parameters
-	options['threshPC']    = 0.9;
-	result_upper = ps.psignifit(data,options);
-	options['threshPC']    = 0.7;
-	result_lower = ps.psignifit(data,options);
+    def fitting():
 
-	# example display all fit values as strings
-	# fit_info = ''
-	# for item in result['Fit']:
-	#	fit_info += str(item) + '\n'
-	# return fit_info
+            faked_data = np.array([[0.10,   45.0000,   90.0000],
+                                     [0.15,   50.0000,   90.0000],
+                                     [0.20,   44.0000,   90.0000],
+                                     [0.25,   44.0000,   90.0000],
+                                     [0.30,   52.0000,   90.0000],
+                                     [0.35,   53.0000,   90.0000],
+                                     [0.40,   62.0000,   90.0000],
+                                     [0.45,   64.0000,   90.0000],
+                                     [0.50,   76.0000,   90.0000],
+                                     [0.60,   79.0000,   90.0000],
+                                     [0.70,   88.0000,   90.0000],
+                                     [0.80,   90.0000,   90.0000],
+                                     [0.90,   90.0000,   90.0000]]);
 
-	upper_thresh = str(result_upper['Fit'][0]) 
-	lower_thresh = str(result_lower['Fit'][0])
+            options = dict();   # initialize as an empty dictionary
+            options['sigmoidName'] = 'norm';   # choose a cumulative Gauss as the sigmoid  
+            options['expType']     = '2AFC';   # choose 2-AFC as the experiment type  
+                                                                               # this sets the guessing rate to .5 (fixed) and  
+                                                                               # fits the rest of the parameters
+            options['threshPC']    = 0.9;
+            result_upper = ps.psignifit(recieved_data,options);
+            options['threshPC']    = 0.7;
+            result_lower = ps.psignifit(recieved_data,options);
 
-	return upper_thresh, lower_thresh 
+            # example display all fit values as strings
+            # fit_info = ''
+            # for item in result['Fit']:
+            #	fit_info += str(item) + '\n'
+            # return fit_info
+            
+            thresholds = [result_upper['Fit'][0],result_lower['Fit'][0]]
+
+            #upper_thresh = str(result_upper['Fit'][0]) 
+            #lower_thresh = str(result_lower['Fit'][0])
+
+            return thresholds
+
+        return jsonify(thresholds)
 
 if __name__ == "__main__":
 	app.run()
