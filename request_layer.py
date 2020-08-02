de9f860a71fe8ee4b616358dd36e0ff369c5d448
dorian <dorian@mino.rs>
2020-08-02 19:55:31 +0100

    remove psignifit config from request_layer so it faithfully relays config to psignifit and result to user - numpy arrays cannot be serialized as JSON however, so this needs to be addressed

--- request_layer.py
+++ request_layer.py
@@ -3,6 +3,7 @@ import argparse # for dealing with command line argument inputs
 # import psignifit resources
 import numpy as np
 import psignifit as ps
+import json
 
 # create options to change host and port
 parser = argparse.ArgumentParser()
@@ -31,23 +32,15 @@ def calculate():
     elif request.content_length is not None and request.content_length > 1024: # let's also limit the size of permitted payloads to 1 KB
         abort(413)
     else: # if JSON, continue
-        recieved_data = request.get_json() # pull the data out of the POST request 
-        converted_data = recieved_data['faked_data'] # format of the POST comes in as a dict, so just select the array
-
-        # set up psignifit with some standard options
-        options = dict();
-        options['sigmoidName'] = 'norm';
-        options['expType']     = '2AFC';
-        # add any options here
+        payload = request.get_json(); # pull the data out of the POST request 
+        data = payload['data']; # format of the POST comes in as a dict, so just select the array
+        options = payload['options'];
 
         # then run psignifit
-        result = ps.psignifit(converted_data,options);
+        result = ps.psignifit(data,options);
 
-        # example: pull threshold value and return it
-        threshold = result['Fit'][0]
-        response = jsonify(threshold) # we must first convert it to JSON
-        return response # then return the JSON for the axios request to pick up
-        # we could do the above in one line if we wanted: `return jsonify(result['Fit'][0])`
+        # then return the result as a JSON object
+        return jsonify(result) 
 
 if __name__ == "__main__":
 	app.run(host=args.host if args.host else None, port=args.port if args.port else None)
