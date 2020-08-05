d0874ad891cf6efdd5b5069852aaeb949b1c806e
Tiziano Zito <opossumnano@gmail.com>
2020-08-05 16:28:27 +0200

    second try at stub for sigmoids test

--- psignifit/tests/test_sigmoids.py
+++ psignifit/tests/test_sigmoids.py
@@ -5,7 +5,7 @@ import psignifit.sigmoids as sg
 
 
 # fixed parameters for simple sigmoid sanity checks
-X = np.linspace(0, 1, num=15)
+X = np.linspace(1e-12, 1-1e-12, num=10000)
 M = 0.5
 WIDTH = 0.9
 PC = 0.5
@@ -18,12 +18,11 @@ ALPHA = 0.05
 # lambda = 1
 # eta = 0
 
-
-ALL_SIGS = [getattr(sg, name) for name in dir(sg) if not name.startswith('_')]
-print(ALL_SIGS)
+# list of all sigmoids (after having removed aliases)
+ALL_SIGS = { getattr(sg, name) for name in dir(sg) if not name.startswith('_') }
 
 @pytest.mark.parametrize('sigmoid', ALL_SIGS)
 def test_sigmoid_sanity_check(sigmoid):
     out = sigmoid(X, M, WIDTH, PC=PC, alpha=ALPHA)
-
+    print(sigmoid.__name__, out.min(), out.max())
 
