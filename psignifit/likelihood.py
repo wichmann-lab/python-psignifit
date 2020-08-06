Fix numerical problems in cleaned log_posterior

psignifit/likelihood.py
@@ -1,8 +1,11 @@
 # -*- coding: utf-8 -*-
+from typing import Dict
+
 import numpy as np
 import scipy.special as sp
 
 from .utils import fp_error_handler, PsignifitException
+from .typing import Sigmoid, Prior, ParameterGrid
 
 # this is the order in which the parameters are stored in the big
 # likelihood 5 dimensional matrix
@@ -10,7 +13,7 @@ PARM_ORDER = ('threshold', 'width', 'lambda', 'gamma', 'eta')
 
 
 def likelihood(data, sigmoid=None, priors=None, grid=None):
-    p = log_likelihood(data, sigmoid=sigmoid, priors=priors, grid=grid)
+    p = log_posterior(data, sigmoid=sigmoid, priors=priors, grid=grid)
     # locate the maximum
     ind_max = np.unravel_index(p.argmax(), p.shape)
     logmax = p[ind_max]
@@ -22,7 +25,24 @@ def likelihood(data, sigmoid=None, priors=None, grid=None):
 
 
 @fp_error_handler(divide='ignore')
-def log_likelihood(data, sigmoid=None, priors=None, grid=None):
+def log_posterior(data: np.ndarray, sigmoid: Sigmoid, priors: Dict[str, Prior], grid: ParameterGrid) -> np.ndarray:
+    """ Estimate the logarithmic posterior probability of sigmoid parameters.
+
+    The function estimates the log-likelihood for a grid of sigmoid parameters and adds the corresponding
+    log-priors. This is described in formulas A.4 and A.5 of [Schuett2016]_.
+
+    Args:
+        data: numpy array with three columns for the stimulus levels, the correct trials, and the total trials.
+        sigmoid: sigmoid function with three input parameter, see :module:`~psignifit.sigmoids`.
+        priors: dictionary of parameter names and corresponding prior function, see :module:`~psignifit.priors`.
+        grid: dictionary of parameter name and numpy array with parameter values.
+    Returns:
+        Numpy array with the log-posterior for each parameter combination in grid.
+
+    .. [Schuett2016] Schütt, H. H., Harmeling, S., Macke, J. H. and Wichmann, F. A. (2016).
+          Painfree and accurate Bayesian estimation of psychometric functions for (potentially) overdispersed data.
+          Vision Research 122, 105-123.
+    """
     thres = grid['threshold']
     width = grid['width']
     lambd = grid['lambda']
@@ -39,15 +59,12 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
     # concatenating?
     # fixing this would also fix the case with the curried likelihood
     # below, that currently does work for grid['eta'] -> None
+
+    # IN WHICH CASE IS ETA == NONE??? AT LEAST NEVER IN BOUNDS (constant)
     if grid['eta'] is None:
         eta_prime = None
-        thres, width, lambd, gamma = np.meshgrid(thres,
-                                                 width,
-                                                 lambd,
-                                                 gamma,
-                                                 copy=False,
-                                                 sparse=True,
-                                                 indexing='ij')
+        thres, width, lambd, gamma = np.meshgrid(thres, width, lambd, gamma,
+                                                 copy=False, sparse=True, indexing='ij')
     else:
         eta_std = grid['eta']
         eta = eta_std ** 2  # use variance instead of standard deviation
@@ -55,14 +72,8 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
 
         # for smaller variance we use the binomial model
         vbinom = (eta <= 1e-09).sum()
-        thres, width, lambd, gamma, eta_prime = np.meshgrid(thres,
-                                                            width,
-                                                            lambd,
-                                                            gamma,
-                                                            eta_prime,
-                                                            copy=False,
-                                                            sparse=True,
-                                                            indexing='ij')
+        thres, width, lambd, gamma, eta_prime = np.meshgrid(thres, width, lambd, gamma, eta_prime,
+                                                            copy=False, sparse=True, indexing='ij')
 
     scale = 1 - gamma - lambd
     ###FIXME: handle the case with equal_asymptote
@@ -75,27 +86,28 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
     # performance by using an explicit, but no profiling has been done to prove
     # this.
     for level, trials, correct_trials in zip(levels, ntrials, ncorrect):
-        # Notation in paper: n=trials, k=correct_trials, x=level
         if trials == 0:
             continue
 
-        # average predicted probability of correct
         psi = sigmoid(level, thres, width) * scale + gamma
-        pbin += correct_trials * np.log(psi) + (trials - correct_trials) * np.log(1 - psi)
+        pbin += correct_trials * np.nan_to_num(np.log(psi)) + (trials - correct_trials) * np.nan_to_num(np.log(1 - psi))
         if eta_prime is not None:
             a = psi * eta_prime
             b = (1 - psi) * eta_prime
-            p += -sp.gammaln(trials + eta_prime) + sp.gammaln(eta_prime)
-
+            # Separate cases to avoid numerical problems with sp.gammaln(...) == inf
+            # for correct_trials == 0 or (trials - correct_trials) == 0
             if correct_trials == 0:
-                p += (sp.gammaln(trials + b) - sp.gammaln(b))
+                p += (sp.gammaln(trials + b) - sp.gammaln(trials + eta_prime) - sp.gammaln(b) + sp.gammaln(eta_prime))
             elif correct_trials == trials:
-                p += (sp.gammaln(correct_trials + a) - sp.gammaln(a))
+                p += (sp.gammaln(correct_trials + a) - sp.gammaln(trials + eta_prime) - sp.gammaln(a) +
+                      sp.gammaln(eta_prime))
             elif correct_trials < trials:
-                p += (sp.gammaln(correct_trials + a) + sp.gammaln(trials - correct_trials + b)
-                      - sp.gammaln(a) - sp.gammaln(b))
-            else: # we should never land here: we can't more ncorrect than ntrials
-                raise PsignifitException('ncorrect %d > ntrials %d!' % (correct_trials, trials))
+                p += (sp.gammaln(correct_trials + a) + sp.gammaln(trials - correct_trials + b) -
+                      sp.gammaln(trials + eta_prime) - sp.gammaln(a) - sp.gammaln(b) +
+                      sp.gammaln(eta_prime))
+        else:
+            # we should never land here: we can't more ncorrect than ntrials
+            raise PsignifitException('ncorrect %d > ntrials %d!' % (correct_trials, trials))
 
     if eta_prime is None:
         p = pbin
@@ -103,8 +115,7 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
         pbin = np.broadcast_to(pbin, pbin.shape[:4] + (vbinom,))
         p = np.concatenate((pbin, p), axis=4)
 
-    # add priors on the corresponding axis (they get added on the right axis
-    # because of the meshgrid
+    # add priors on the corresponding axis
     p += np.log(priors['threshold'](thres))
     p += np.log(priors['width'](width))
     p += np.log(priors['lambda'](lambd))
@@ -117,7 +128,7 @@ def log_likelihood(data, sigmoid=None, priors=None, grid=None):
 
 def get_optm_llh(data, sigmoid=None, priors=None, grid=None):
     ###FIXME### does not work when grid[parm] = None!!!
-    # return a curried version of log_likelihood to be used in the
+    # return a curried version of log_posterior to be used in the
     # optimization routine
     # - go through the grid and detect "fixed" parameters that we shouldn't
     #   try to optimize
@@ -133,6 +144,6 @@ def get_optm_llh(data, sigmoid=None, priors=None, grid=None):
             y.insert(fidx, fval)
         # create a temporary grid with only one step (all params are fixed)
         lgrid = {parm: y[idx] for idx, parm in enumerate(PARM_ORDER)}
-        return -log_likelihood(data, sigmoid=sigmoid, priors=priors, grid=lgrid)
+        return -log_posterior(data, sigmoid=sigmoid, priors=priors, grid=lgrid)
 
     return cllh, fixed