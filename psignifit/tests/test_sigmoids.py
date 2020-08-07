4129e77d8d827cd749ebb1a3ca7030d1d3abe7c3
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-08-07 12:44:50 +0200

    Fix sanity checks for sigmoid functions
    Tests the relations between M, PC and WIDTH, ALPHA.

--- psignifit/tests/test_sigmoids.py
+++ psignifit/tests/test_sigmoids.py
@@ -19,18 +19,27 @@ ALPHA = 0.05
 # eta = 0
 
 # list of all sigmoids (after having removed aliases)
-ALL_SIGS = { getattr(sg, name) for name in dir(sg) if not name.startswith('_') }
-
 LOG_SIGS = ('weibull', 'logn', 'neg_weibull', 'neg_logn')
+ALL_SIGS = [ (getattr(sg, name), name in LOG_SIGS, 'neg' in name)
+             for name in dir(sg) if not name.startswith('_')]
 
-@pytest.mark.parametrize('sigmoid', ALL_SIGS)
-def test_sigmoid_sanity_check(sigmoid):
-    #if sigmoid.__name__ in LOG_SIGS:
-    #    x = np.log(X)
-    #else:
-    #    x = X
-    x = X
-    out = sigmoid(x, M, WIDTH, PC=PC, alpha=ALPHA)
-    #print(sigmoid.__name__, out.min(), out.max())
-    assert np.isclose(sigmoid(M, M, WIDTH, PC=PC, alpha=ALPHA), PC)
 
+@pytest.mark.parametrize('sigmoid,is_log,is_neg', ALL_SIGS)
+def test_sigmoid_sanity_check(sigmoid, is_log, is_neg):
+    x_M = M
+    x = np.linspace(1e-8, 1, 100)
+    if is_neg:
+        x = 1 - x
+    if is_log:
+        x_M = np.exp(x_M)
+        x = np.exp(x)
+
+    # sigmoid(M) == PC
+    np.testing.assert_allclose(sigmoid(x_M, M, WIDTH, PC=PC, alpha=ALPHA), PC)
+
+    # |X_L - X_R| == WIDTH, with
+    # with sigmoid(X_L) == ALPHA
+    # and  sigmoid(X_R) == 1 - ALPHA
+    s = sigmoid(x, M, WIDTH, PC=PC, alpha=ALPHA)
+    idx_alpha, idx_nalpha =  np.abs(s - ALPHA).argmin(), np.abs(s - (1 - ALPHA)).argmin()
+    np.testing.assert_allclose(s[idx_nalpha] - s[idx_alpha], WIDTH, atol=0.02)
