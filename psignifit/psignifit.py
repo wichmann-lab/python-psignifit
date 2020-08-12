1693550253f8c17cea5cd230b835f5f33e7de9d2
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-08-12 14:42:15 +0200

    Move parameter fitting and data checking to separate functions.

--- psignifit/psignifit.py
+++ psignifit/psignifit.py
@@ -44,81 +44,36 @@ def psignifit(data, conf=None, **kwargs):
     """
     if conf is None:
         conf = Conf(**kwargs)
-    elif isinstance(conf, dict):
-        conf = Conf(**conf)
     elif len(kwargs) > 0:
         # user shouldn't specify a conf object *and* kwargs simultaneously
         raise PsignifitException(
             "Can't handle conf together with other keyword arguments!")
 
-    verbose = conf.verbose
-
-    data = np.asarray(data, dtype=float)
-
-    # check that the data are plausible
+    data = _check_data(data, verbose=conf.verbose, logspace=conf._logspace,
+                       has_user_stimulus_range=conf.stimulus_range is not None)
     levels, ncorrect, ntrials = data[:, 0], data[:, 1], data[:, 2]
 
-    # levels should show some variance
-    if levels.max() == levels.min():
-        raise PsignifitException('Your stimulus levels are all identical.'
-                                 ' They can not be fitted by a sigmoid!')
-
-    # ncorrect and ntrials should be integers
-    if not np.allclose(ncorrect, ncorrect.astype(int)):
-        raise PsignifitException(
-            'The number correct column contains non integer'
-            ' numbers!')
-
-    if not np.allclose(ntrials, ntrials.astype(int)):
-        raise PsignifitException('The number of trials column contains non'
-                                 ' integer numbers!')
-
-    # options
-
-    # if options.experiment_type in ['2AFC', '3AFC', '4AFC']:
-    #    options.experiment_choices = int(float(options.experiment_type[0]))
-    #    options.experiment_type = ExperimentType.N_AFC
-
-    # if options.experiment_type == ExperimentType.N_AFC and not('experiment_choices' in options.keys()):
-    #    raise ValueError('For nAFC experiments please also pass the number of alternatives (options.experiment_choices)')
-
-    # log space sigmoids
-    # we fit these functions with a log transformed physical axis
-    # This is because it makes the paramterization easier and also the priors
-    # fit our expectations better then.
-    # The flag is needed for the setting of the parameter bounds in setBounds
-
-    # options['sigmoidName'] in ['Weibull','logn','weibull']:
-    #        options['logspace'] = 1
-    #        assert min(data[:,0]) > 0, 'The sigmoid you specified is not defined for negative data points!'
-    # else:
-    #    options['logspace'] = 0
-
-    # stimulus range settings
     stimulus_range = conf.stimulus_range
     if stimulus_range is None:
-        # derive the stimulus range from the data
         stimulus_range = (levels.min(), levels.max())
     if conf._logspace:
-        # change it to logspace if needed
         stimulus_range = (np.log(stimulus_range[0]), np.log(stimulus_range[1]))
+        levels = np.log(levels)
 
-    # width_min is the minimum difference between two different stimulus levels
     width_min = conf.width_min
     if width_min is None:
-        # if it wasn't overriden by the user, derive it from the data
         if conf.stimulus_range is None:
-            # if the stimulus range is spanned by the input data
-            if conf._logspace:
-                width_min = np.diff(np.unique(np.log(levels))).min()
-            else:
-                width_min = np.diff(np.unique(levels)).min()
+            width_min = np.diff(np.unique(levels)).min()
         else:
-            # if the stimulus range was set manually, we can not derive width_min
-            # from the data, so we will instead use a very conservative estimate,
-            # i.e. 100 ULP from the largest end of the stimulus range
-            # for ULP, see https://en.wikipedia.org/wiki/Unit_in_the_last_place
+            # For user specified stimulus range, use very conservative estimate of width_min.
+            # https: // en.wikipedia.org / wiki / Unit_in_the_last_place
             width_min = 100 * np.spacing(stimulus_range[1])
+
+    if ntrials.max() == 1 or len(levels) > conf.pool_max_blocks:
+        warnings.warn("""Pooling data, to avoid problems with n=1 blocks or to save time fitting
+            because you have more than 25 blocks.
+            You can force acceptance of your blocks by increasing conf.pool_max_blocks""")
+        return pool_data(data, xtol=conf.pool_xtol, max_gap=conf.pool_max_gap, max_length=conf.pool_max_length)
 
     priors = _priors.default_priors(stimulus_range, width_min, conf.width_alpha,
                                     conf.beta_prior, thresh_PC=conf.thresh_PC)
@@ -126,91 +81,22 @@ def psignifit(data, conf=None, **kwargs):
         priors.update(conf.priors)
     _priors.check_priors(priors, stimulus_range, width_min)
 
-    # sigmoid
-    sigmoid = getattr(sigmoids, conf.sigmoid)
-    # fix thresh_PC and width_alpha parameters for the sigmoid
-    sigmoid = partial(sigmoid, PC=conf.thresh_PC, alpha=conf.width_alpha)
-
-    # warning if many blocks were measured
-    if verbose and len(levels) >= 25 and not conf.stimulus_range:
-        print(
-            f"""The data you supplied contained {len(levels)}>= 25 stimulus levels.
-Did you sample adaptively?
-If so please specify a range which contains the whole psychometric function in
-conf.stimulus_range.
-An appropriate prior prior will be then chosen. For now we use the standard
-heuristic, assuming that the psychometric function is covered by the stimulus
-levels,which is frequently invalid for adaptive procedures!""")
-
-    if verbose and ntrials.max() <= 5 and not conf.stimulus_range:
-        print("""All provided data blocks contain <= 5 trials.
-            Did you sample adaptively?
-            If so please specify a range which contains the whole psychometric function in
-            conf.stimulus_range.
-            An appropriate prior prior will be then chosen. For now we use the standard
-            heuristic, assuming that the psychometric function is covered by the stimulus
-            levels, which is frequently invalid for adaptive procedures!""")
-
-    if verbose and ntrials.max() == 1 or len(levels) > conf.pool_max_blocks:
-        print(
-            """Pooling data, to avoid problems with n=1 blocks or to save time fitting
-            because you have more than 25 blocks.
-            You can force acceptance of your blocks by increasing conf.pool_max_blocks"""
-        )
-        data = pool_data(data,
-                         xtol=conf.pool_xtol,
-                         max_gap=conf.pool_max_gap,
-                         max_length=conf.pool_max_length)
-
     bounds = parameter_bounds(wmin=width_min, etype=conf.experiment_type, srange=stimulus_range,
                               alpha=conf.width_alpha, echoices = conf.experiment_choices)
-
-    # override at user request
     if conf.bounds is not None:
         bounds.update(conf.bounds)
-
     if conf.fixed_parameters is not None:
         for param, value in conf.fixed_parameters.items():
             bounds[param] = (value, value)
+
+    sigmoid = getattr(sigmoids, conf.sigmoid)
+    sigmoid = partial(sigmoid, PC=conf.thresh_PC, alpha=conf.width_alpha)
 
     # normalize priors to first choice of bounds
     for parameter, prior in priors.items():
         priors[parameter] = normalize(prior, bounds[parameter])
 
-    # do first sparse grid posterior_grid evaluation
-    grid = get_grid(bounds, conf.steps_moving_bounds)
-    posteriors_sparse, grid_max = posterior_grid(data, sigmoid=sigmoid, priors=priors, grid=grid)
-    # normalize the posterior_grid
-    posterior_volumes = posteriors_sparse * integral_weights([grid_value for _, grid_value in sorted(grid.items())])
-    posterior_integral = posterior_volumes.sum()
-
-    # indices on the grid of the volumes that contribute more than `tol` to the overall integral
-    mask = np.nonzero(posterior_volumes / posterior_integral >= conf.max_bound_value)
-    for idx, parm in enumerate(sorted(bounds.keys())):
-        pgrid = grid[parm]
-        # get the indeces for this parameter's axis and sort it
-        axis = np.sort(mask[idx])
-        # new bounds are the extrema of this axis, but enlarged of one element
-        # in both directions
-        left = max(0, axis[0] - 1)
-        right = min(axis[-1] + 1, len(pgrid) - 1)
-        # update the bounds
-        bounds[parm] = (pgrid[left], pgrid[right])
-
-    # do dense grid posterior_grid evaluation
-    grid = get_grid(bounds, conf.grid_steps)
-
-    posteriors, grid_max = posterior_grid(data, sigmoid=sigmoid, priors=priors, grid=grid)
-    print('fit0', grid_max)
-
-    fixed_param = {}
-    for parm_name, parm_values in grid.items():
-        if parm_values is None:
-            fixed_param[parm_name] = parm_values
-        elif len(parm_values) <= 1:
-            fixed_param[parm_name] = parm_values[0]
-
-    fit_dict = max_posterior(data, param_init=grid_max, param_fixed=fixed_param, sigmoid=sigmoid, priors=priors)
+    fit_dict = _fit_parameters(data, bounds, priors, sigmoid, conf.steps_moving_bounds, conf.max_bound_value, conf.grid_steps)
     results = {'sigmoid_parameters': fit_dict}
 
     # take care of confidence intervals/condifence region
@@ -254,6 +140,96 @@ levels,which is frequently invalid for adaptive procedures!""")
     # plot.plotPsych(result)
 
     return results
+
+
+def _fit_parameters(data: np.ndarray, bounds: ParameterBounds,
+                    priors: Dict[str, Prior], sigmoid: Sigmoid,
+                    steps_moving_bounds: Dict[str, int], max_bound_value: float,
+                    grid_steps: Dict[str, int]) -> Dict[str, float]:
+    """ Fit sigmoid parameters in a three step procedure. """
+    # do first sparse grid posterior_grid evaluation
+    grid = get_grid(bounds, steps_moving_bounds)
+    posteriors_sparse, grid_max = posterior_grid(data, sigmoid=sigmoid, priors=priors, grid=grid)
+    # normalize the posterior_grid
+    posterior_volumes = posteriors_sparse * integral_weights([grid_value for _, grid_value in sorted(grid.items())])
+    posterior_integral = posterior_volumes.sum()
+    # indices on the grid of the volumes that contribute more than `tol` to the overall integral
+    mask = np.nonzero(posterior_volumes / posterior_integral >= max_bound_value)
+    for idx, parm in enumerate(sorted(bounds.keys())):
+        pgrid = grid[parm]
+        # get the indeces for this parameter's axis and sort it
+        axis = np.sort(mask[idx])
+        # new bounds are the extrema of this axis, but enlarged of one element
+        # in both directions
+        left = max(0, axis[0] - 1)
+        right = min(axis[-1] + 1, len(pgrid) - 1)
+        # update the bounds
+        bounds[parm] = (pgrid[left], pgrid[right])
+    # do dense grid posterior_grid evaluation
+    grid = get_grid(bounds, grid_steps)
+    posteriors, grid_max = posterior_grid(data, sigmoid=sigmoid, priors=priors, grid=grid)
+    print('fit0', grid_max)
+    fixed_param = {}
+    for parm_name, parm_values in grid.items():
+        if parm_values is None:
+            fixed_param[parm_name] = parm_values
+        elif len(parm_values) <= 1:
+            fixed_param[parm_name] = parm_values[0]
+    fit_dict = max_posterior(data, param_init=grid_max, param_fixed=fixed_param, sigmoid=sigmoid, priors=priors)
+    return fit_dict
+
+
+def _check_data(data: np.ndarray, verbose: bool, logspace: bool, has_user_stimulus_range: bool) -> np.ndarray:
+    """ Check data format, type and range.
+
+    Args:
+        data: The data matrix with columns levels, number of correct and number of trials
+        verbose: Print warnings
+        logspace: Data should be used logarithmically
+        is_user_stimulus_range: User configured the stimulus range
+    Returns:
+        data as float numpy array
+    """
+    data = np.asarray(data, dtype=float)
+    if len(data.shape) != 2 and data.shape[1] != 3:
+        raise PsignifitException("Expects data to be two dimensional with three columns, got {data.shape = }")
+    levels, ncorrect, ntrials = data[:, 0], data[:, 1], data[:, 2]
+
+    # levels should show some variance
+    if levels.max() == levels.min():
+        raise PsignifitException('Your stimulus levels are all identical.'
+                                 ' They can not be fitted by a sigmoid!')
+    # ncorrect and ntrials should be integers
+    if not np.allclose(ncorrect, ncorrect.astype(int)):
+        raise PsignifitException(
+            'The number correct column contains non integer'
+            ' numbers!')
+    if not np.allclose(ntrials, ntrials.astype(int)):
+        raise PsignifitException('The number of trials column contains non'
+                                 ' integer numbers!')
+    if logspace and levels.min() <= 0:
+        raise PsignifitException(f'Sigmoid {data.sigmoid} expects positive stimulus level data.')
+
+    # warning if many blocks were measured
+    if verbose and len(levels) >= 25 and not has_user_stimulus_range:
+        warnings.warn(f"""The data you supplied contained {len(levels)}>= 25 stimulus levels.
+            Did you sample adaptively?
+            If so please specify a range which contains the whole psychometric function in
+            conf.stimulus_range.
+            An appropriate prior prior will be then chosen. For now we use the standard
+            heuristic, assuming that the psychometric function is covered by the stimulus
+            levels,which is frequently invalid for adaptive procedures!""")
+
+    if verbose and ntrials.max() <= 5 and not is_user_stimulus_range:
+        warnings.warn("""All provided data blocks contain <= 5 trials.
+            Did you sample adaptively?
+            If so please specify a range which contains the whole psychometric function in
+            conf.stimulus_range.
+            An appropriate prior prior will be then chosen. For now we use the standard
+            heuristic, assuming that the psychometric function is covered by the stimulus
+            levels, which is frequently invalid for adaptive procedures!""")
+    return data
+
 
 
 def psignifitFast(data, options):
