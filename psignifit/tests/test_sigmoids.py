c97691d64b76daebb36753ef189ff7c9a036dc51
David-Elias Kuenstle <david-elias.kuenstle@uni-tuebingen.de>
2020-11-03 10:31:55 +0100

    Test sigmoid slopes, improve sigmoid inverse tests

--- psignifit/tests/test_sigmoids.py
+++ psignifit/tests/test_sigmoids.py
@@ -42,26 +42,47 @@ def test_sigmoid_by_name(sigmoid_name):
 
 @pytest.mark.parametrize('sigmoid_name', sigmoids.ALL_SIGMOID_NAMES)
 def test_sigmoid_sanity_check(sigmoid_name):
-    sigmoid = sigmoids.sigmoid_by_name(sigmoid_name, PC=PC, alpha=ALPHA)
-    x_threshold = THRESHOLD_PARAM
-    x = np.linspace(1e-8, 1, 100)
-    if sigmoid.negative:
-        x = 1 - x
-    if sigmoid.logspace:
-        x_threshold = np.exp(x_threshold)
-        x = np.exp(x)
+    """ Basic sanity checks for sigmoids.
 
-    # sigmoid(M) == PC
-    np.testing.assert_allclose(sigmoid(x_threshold, THRESHOLD_PARAM, WIDTH_PARAM), PC)
+    These sanity checks test some basic relations between the parameters
+    as well as rule of thumbs which can be derived from visual inspection
+    of the sigmoid functions.
+    """
+    sigmoid = sigmoids.sigmoid_by_name(sigmoid_name, PC=PC, alpha=ALPHA)
+    threshold_stimulus_level = THRESHOLD_PARAM
+    stimulus_levels = np.linspace(1e-8, 1, 100)
+    if sigmoid.negative:
+        stimulus_levels = 1 - stimulus_levels
+    if sigmoid.logspace:
+        threshold_stimulus_level = np.exp(threshold_stimulus_level)
+        stimulus_levels = np.exp(stimulus_levels)
+
+    # sigmoid(threshold_stimulus_level) == threshold_percent_correct
+    np.testing.assert_allclose(sigmoid(threshold_stimulus_level, THRESHOLD_PARAM, WIDTH_PARAM), PC)
 
     # |X_L - X_R| == WIDTH, with
     # with sigmoid(X_L) == ALPHA
     # and  sigmoid(X_R) == 1 - ALPHA
-    s = sigmoid(x, THRESHOLD_PARAM, WIDTH_PARAM)
-    idx_alpha, idx_nalpha =  np.abs(s - ALPHA).argmin(), np.abs(s - (1 - ALPHA)).argmin()
-    np.testing.assert_allclose(s[idx_nalpha] - s[idx_alpha], WIDTH_PARAM, atol=0.02)
+    perc_correct = sigmoid(stimulus_levels, THRESHOLD_PARAM, WIDTH_PARAM)
+    idx_alpha, idx_nalpha =  np.abs(perc_correct - ALPHA).argmin(), np.abs(perc_correct - (1 - ALPHA)).argmin()
+    np.testing.assert_allclose(perc_correct[idx_nalpha] - perc_correct[idx_alpha], WIDTH_PARAM, atol=0.02)
 
-    t = sigmoid.inverse(PC, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM)
-    np.testing.assert_allclose(t, x_threshold)
-    t = sigmoid.inverse(PC, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM, gamma=0, lambd=0)
-    np.testing.assert_allclose(t, x_threshold)
+    # Inverse sigmoid at threshold percentage correct (y-axis)
+    # Expects the threshold stimulus level (x-axis).
+    stimulus_level_from_inverse = sigmoid.inverse(PC, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM)
+    np.testing.assert_allclose(stimulus_level_from_inverse, threshold_stimulus_level)
+    stimulus_level_from_inverse = sigmoid.inverse(PC, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM, gamma=0, lambd=0)
+    np.testing.assert_allclose(stimulus_level_from_inverse, threshold_stimulus_level)
+    # Expects inverse(value(x)) == x
+    y = sigmoid(stimulus_levels, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM)
+    np.testing.assert_allclose(stimulus_levels, sigmoid.inverse(y, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM), atol=1e-9)
+
+    slope = sigmoid.slope(stimulus_levels, threshold=THRESHOLD_PARAM, width=WIDTH_PARAM, gamma=0, lambd=0)
+    # Expects maximal slope at a medium stimulus level
+    assert 0.4 * len(slope) < np.argmax(np.abs(slope)) < 0.6 * len(slope)
+    # Expects slope to be all positive/negative for standard/decreasing sigmoid
+    if sigmoid.negative:
+        assert np.all(slope < 0)
+    else:
+        assert np.all(slope > 0)
+
