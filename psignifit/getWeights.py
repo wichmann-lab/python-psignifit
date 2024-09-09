3c11e3908851d978bd25a31bed7bf7fe82730817
Pietro Berkes <pietro.berkes@googlemail.com>
2024-09-09 13:07:35 +0200

    Import `diff` from `numpy`, not `scipy`

--- psignifit/getWeights.py
+++ psignifit/getWeights.py
@@ -6,31 +6,30 @@ this function calculates the weights for integration/the volume of the
 cuboids given by the 1 dimensional borders in X1D
 
 """
-from numpy import tile, reshape, ones, array, newaxis, where, multiply, convolve
-from scipy import diff
+from numpy import diff, tile, reshape, ones, array, newaxis, where, multiply, convolve
 from scipy.signal import convolve as convn
 
 def getWeights(X1D):
-    
+
     d = len(X1D)
-    
+
     ''' puts the X values in their respective dimensions to use bsxfun for
     evaluation'''
     Xreshape = []
     Xreshape.append(X1D[0].reshape(-1,1))
     if d >= 2:
         Xreshape.append(X1D[1].reshape([1,-1]))
-    
+
     for idx in range (2,d):
         dims = [1]*idx
         dims.append(-1)
         Xreshape.append(reshape(X1D[idx], dims))
-    
-    
+
+
     # to find and handle singleton dimensions
     sizes = [X1D[ix].size for ix in range(0,d)]
     Xlength = array(sizes)
-    
+
     ''' calculate weights '''
     #1) calculate mass/volume for each cuboid
     weight = 1
