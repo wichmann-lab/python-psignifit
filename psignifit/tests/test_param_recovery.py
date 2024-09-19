637091c6962edeca475687ed1628522399150dc3
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2024-09-19 17:29:26 +0200

    unfinished tests added for fitting subsets of parameters and simulating with eta

--- psignifit/tests/test_param_recovery.py
+++ psignifit/tests/test_param_recovery.py
@@ -1,8 +1,11 @@
 import numpy as np
 import pytest
+from scipy import stats
 
 from psignifit import psignifit
 from psignifit._sigmoids import Gaussian, ALL_SIGMOID_NAMES, sigmoid_by_name
+
+RANDOMSTATE = np.random.RandomState(837400)
 
 
 def psychometric(stimulus_level, threshold, width, gamma, lambda_,
@@ -41,6 +44,25 @@ def psychometric(stimulus_level, threshold, width, gamma, lambda_,
     return psi
 
 
+def psychometric_with_eta(stimulus_level, threshold, width, gamma, lambda_,
+                 sigmoid_name, eta, random_state=np.random.RandomState(42)):
+
+    # we use the defaults for pc and alpha in the sigmoids:
+    # pc = 0.5
+    # alpha = 0.05
+    sigmoid = sigmoid_by_name(sigmoid_name)
+    sigmoid_values = sigmoid(stimulus_level, threshold=threshold, width=width)
+    psi = gamma + (1.0 - lambda_ - gamma) * sigmoid_values
+    new_psi = []
+    for p in psi:
+        a = ((1/eta**2) - 1) * p
+        b = ((1/eta**2) - 1) * (1 - p)
+        noised_p = stats.beta.rvs(a=a, b=b, size=1, random_state=random_state)
+        new_psi.append(noised_p)
+    return np.array(new_psi).squeeze()
+
+
+
 @pytest.mark.parametrize("sigmoid", list(ALL_SIGMOID_NAMES))
 def test_parameter_recovery_2afc(sigmoid):
     width = 0.3
@@ -56,7 +78,6 @@ def test_parameter_recovery_2afc(sigmoid):
                             sigmoid)
     ntrials = np.ones(nsteps) * 9000000
     hits = (perccorr * ntrials).astype(int)
-    # data
     data = np.dstack([stimulus_level, hits, ntrials]).squeeze()
 
     options = {}
@@ -76,16 +97,98 @@ def test_parameter_recovery_2afc(sigmoid):
 
 
 
+@pytest.mark.parametrize("sigmoid", list(ALL_SIGMOID_NAMES))
+@pytest.mark.parametrize("eta", [0.1, 0.2, 0.3])
+def test_parameter_recovery_2afc_eta(eta, sigmoid):
+    #sigmoid="norm"
+    width = 0.3
+    stim_range = [0.001, 0.001 + width * 1.1]
+    threshold = stim_range[1]/3
+    gamma = 0.5  # 2-AFC
+    lambda_ = 0.0232
 
+    nsteps = 100
+    stimulus_level = np.linspace(stim_range[0], stim_range[1], nsteps)
+
+    perccorr = psychometric_with_eta(stimulus_level, threshold, width, gamma, lambda_,
+                            sigmoid, eta=0.2, random_state=RANDOMSTATE)
+
+    ntrials = np.ones(nsteps) * 500
+    hits = (perccorr * ntrials).astype(int)
+    data = np.dstack([stimulus_level, hits, ntrials]).squeeze()
+
+    options = {}
+    options['sigmoid'] = sigmoid  # choose a cumulative Gauss as the sigmoid
+    options['experiment_type'] = '2AFC'
+    options['fixed_parameters'] = {'lambda': lambda_,
+                                   'gamma': gamma}
+    options["stimulus_range"] = stim_range
+
+    res = psignifit(data, **options)
+
+    assert np.isclose(res.parameter_estimate['lambda'], lambda_)
+    assert np.isclose(res.parameter_estimate['gamma'], gamma)
+    assert np.isclose(res.parameter_estimate['eta'], eta, atol=0.3)
+    assert np.isclose(res.parameter_estimate['threshold'], threshold, atol=0.05)
+    assert np.isclose(res.parameter_estimate['width'], width, atol=0.07)
+
+
+#@pytest.mark.parametrize("sigmoid", list(ALL_SIGMOID_NAMES))
+def test_parameter_recovery_2afc_fixed_params():
+    sigmoid = "norm"
+    all_possible_params = ['lambda', 'gamma', 'eta', 'threshold', 'width']
+    width = 0.3
+    stim_range = [0.001, 0.001 + width * 1.1]
+    nsteps = 50
+    sim_params = {
+        "width" : width,
+        "stim_range" : stim_range,
+        "threshold" : stim_range[1]/3,
+        "gamma" : 0.5,  # 2-AFC
+        "lambda_" : 0.0232,
+        "nsteps" : nsteps,
+        "eta": 0,
+        "stimulus_level" : np.linspace(stim_range[0], stim_range[1], nsteps)
+    }
+
+    perccorr = psychometric(sim_params["stimulus_level"],
+                            sim_params["threshold"],
+                            sim_params["width"],
+                            sim_params["gamma"],
+                            sim_params["lambda_"],
+                            sigmoid)
+
+    ntrials = np.ones(nsteps) * 9000000
+    hits = (perccorr * ntrials).astype(int)
+    data = np.dstack([sim_params["stimulus_level"], hits, ntrials]).squeeze()
+
+    fixed_params = [['lambda_', 'gamma', 'eta'], ['lambda_', 'gamma']]
+
+    for fp in fixed_params:
+        options = {}
+        options['sigmoid'] = sigmoid  # choose a cumulative Gauss as the sigmoid
+        options['experiment_type'] = '2AFC'
+        options["stimulus_range"] = stim_range
+        options['fixed_parameters'] = {}
+        for p in fp:
+            options['fixed_parameters'][p] = sim_params[p]
+        print(options['fixed_parameters'])
+
+        res = psignifit(data, **options)
+        # check that all the fixed parameters are unchanged
+        for p in fp:
+            assert np.isclose(res.parameter_estimate[p], sim_params[p], atol=1e-10)
+        # check that the other parameters are close to the true values
+        for p in all_possible_params:
+            if p not in fp:
+                assert np.isclose(res.parameter_estimate[p], sim_params[p], atol=1e-4)
 
 
     # TODO: Also check for warnings
     # TODO: check different fixed params and different options
     # TODO: add simulation test for Y/N paradigm
-    # TODO: simulation test with different ETAs?
-    
-    
-    
+
+
     # 1 block per stimulus level
     # number of trials no should be (rule of thumb) 40-50 trials. No more than 200, since it assymptotes
     
