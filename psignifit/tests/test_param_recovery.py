20d64a86c026ee6eacd93af5cbb104e550fab6be
Lisa Schwetlick <lisa.schwetlick@uni-potsdam.de>
2024-09-19 14:59:30 +0200

    generalizes the test

--- psignifit/tests/test_param_recovery.py
+++ psignifit/tests/test_param_recovery.py
@@ -1,45 +1,91 @@
-from psignifit._sigmoids import Gaussian
 import numpy as np
 import pytest
+
 from psignifit import psignifit
+from psignifit._sigmoids import Gaussian, ALL_SIGMOID_NAMES, sigmoid_by_name
 
-def test_parameter_recovery():
-    stim_range = [0.001, 0.01]
-    nsteps = 19
-    threshold = stim_range[1]/2
-    # we want the threshold to be in the middle of the range, but I dont know
-    # how to get the scaled sigmoid directly.
-    PC = 0.5
-    # alpha is related to the width: width = X_(1-alpha) - X_(alpha)
-    # i dont understand why we are passing both alpha and width then...
-    alpha = 0.05
-    width = 0.01
 
-    # levels column
-    stimulus_level = np.linspace(stim_range[0],stim_range[1],nsteps)
-    # hits and ntrials columns
-    g = Gaussian(PC=PC, alpha=alpha, negative=False, logspace=False)
-    # the +1 and /2 are to convert the range from 0 to 1 to 0.5 to 1
-    perccorr = (g(stimulus_level, threshold=threshold, width=width) + 1) / 2
-    ntrials = np.ones(19)*9000000
+def psychometric(stimulus_level, threshold, width, gamma, lambda_,
+                 sigmoid_name):
+    """ Psychometric function aka percent correct function.
+
+    Generates percent correct values for a range of stimulus levels given a
+    sigmoid.
+    Implementation of Eq 1 in Schuett, Harmeling, Macke and Wichmann (2016)
+
+    Parameters:
+        stimulus_level: array
+          Values of the stimulus value
+        threshold: float
+            Threshold of the psychometric function
+        width: float
+            Width of the psychometric function
+        gamma: float
+            Guess rate
+        lambda_: float
+            Lapse rate
+        sigmoid: callable
+            Sigmoid function to use. Default is Gaussian
+
+    Returns:
+        psi: array
+            Percent correct values for each stimulus level
+
+    """
+    # we use the defaults for pc and alpha in the sigmoids:
+    # pc = 0.5
+    # alpha = 0.05
+    sigmoid = sigmoid_by_name(sigmoid_name)
+    sigmoid_values = sigmoid(stimulus_level, threshold=threshold, width=width)
+    psi = gamma + (1.0 - lambda_ - gamma) * sigmoid_values
+    return psi
+
+
+@pytest.mark.parametrize("sigmoid", list(ALL_SIGMOID_NAMES))
+def test_parameter_recovery_2afc(sigmoid):
+    width = 0.3
+    stim_range = [0.001, 0.001 + width * 1.1]
+    threshold = stim_range[1]/3
+    gamma = 0.5  # 2-AFC
+    lambda_ = 0.0232
+
+    nsteps = 50
+    stimulus_level = np.linspace(stim_range[0], stim_range[1], nsteps)
+
+    perccorr = psychometric(stimulus_level, threshold, width, gamma, lambda_,
+                            sigmoid)
+    ntrials = np.ones(nsteps) * 9000000
     hits = (perccorr * ntrials).astype(int)
     # data
     data = np.dstack([stimulus_level, hits, ntrials]).squeeze()
 
     options = {}
-    options['sigmoid'] = 'norm'  # choose a cumulative Gauss as the sigmoid
+    options['sigmoid'] = sigmoid  # choose a cumulative Gauss as the sigmoid
     options['experiment_type'] = '2AFC'
-    options['fixed_parameters'] = {'lambda': 0,
-                                   'gamma': 0.5}
-    options["stimulus_range"] = [0,0.01]
+    options['fixed_parameters'] = {'lambda': lambda_,
+                                   'gamma': gamma}
+    options["stimulus_range"] = stim_range
 
     res = psignifit(data, **options)
 
-    assert np.isclose(res.parameter_estimate['lambda'], 0)
-    assert np.isclose(res.parameter_estimate['gamma'], 0.5)
+    assert np.isclose(res.parameter_estimate['lambda'], lambda_)
+    assert np.isclose(res.parameter_estimate['gamma'], gamma)
     assert np.isclose(res.parameter_estimate['eta'], 0, atol=1e-4)
     assert np.isclose(res.parameter_estimate['threshold'], threshold, atol=1e-4)
     assert np.isclose(res.parameter_estimate['width'], width, atol=1e-4)
-    
+
+
+
+
+
+
     # TODO: Also check for warnings
+    # TODO: check different fixed params and different options
+    # TODO: add simulation test for Y/N paradigm
+    # TODO: simulation test with different ETAs?
+    
+    
+    
+    # 1 block per stimulus level
+    # number of trials no should be (rule of thumb) 40-50 trials. No more than 200, since it assymptotes
     
