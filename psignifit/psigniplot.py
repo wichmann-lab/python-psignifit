conflict on marker size scaling fixed

psignifit/psigniplot.py
@@ -45,9 +45,7 @@ def plot_psychometric_function(result: Result,  # noqa: C901, this function is t
     x_data = data[:, 0]
     if plot_data:
         y_data = data[:, 1] / data[:, 2]
-        # the size is proportional to the amount of data. Factor 10 is to make
-        # it visually similar to the MATLAB figure version
-        size = np.sqrt(data_size / 2 * data[:, 2])*10
+        size = np.sqrt(data_size / 2 * data[:, 2])
         ax.scatter(x_data, y_data, s=size, color=data_color, marker='.', clip_on=False)
 
     sigmoid = config.make_sigmoid()