aa08645a6e4f0aa3155db67e9f42baab0c58f9cb
Pietro Berkes <pietro.berkes@googlemail.com>
2024-09-24 17:49:42 +0200

    Move `assert_sanity_checks` to tests, and I think I fixed a bug in Sigmoid

--- psignifit/tests/test_sigmoids.py
+++ psignifit/tests/test_sigmoids.py
@@ -4,12 +4,82 @@ import pytest
 from psignifit import sigmoids
 
 
-# fixed parameters for simple sigmoid sanity checks
-X = np.linspace(1e-12, 1 - 1e-12, num=10000)
-THRESHOLD_PARAM = 0.5
-WIDTH_PARAM = 0.9
-PC = 0.5
-ALPHA = 0.05
+def assert_sanity_checks(sigmoid, n_samples: int, threshold: float):
+    """ Assert multiple sanity checks on this sigmoid implementation.
+
+    These checks cannot completely assure the correct implementation of a sigmoid,
+    but try to catch common and obvious mistakes.
+    We recommend to use these for custom sigmoid subclasses.
+
+    The checks are performed on linear spaced stimulus levels between 0 and 1
+    and the provided sigmoid parameters.
+
+    Two checks for relations between parameters:
+      - `sigmoid(threshold_stimulus_level) == threshold_percent_correct`
+      - `|X_L - X_R| == width`
+        with `sigmoid(X_L) == alpha`
+        and  `sigmoid(X_R) == 1 - alpha`
+
+    Two checks for the inverse:
+      - `inverse(PC) == threshold_stimulus_level`
+      - `inverse(inverse(stimulus_levels) == stimulus_levels`
+
+    Two checks for the slope:
+      - `maximum(|slope(stimulus_levels)|)` close to `|slope(0.5)|`
+      - `slope(stimulus_levels) > 0`  (or < 0 for negative sigmoid)
+
+    Args:
+        sigmoid : The Sigmoid subclass to test
+        n_samples: Number of stimulus levels between 0 (exclusive) and 1 for tests
+        threshold: Parameter value for threshold at PC
+        width: Width of the sigmoid
+    Raises:
+        AssertionError if a sanity check fails.
+    """
+    stimulus_levels = np.linspace(-0.3, 1.3, n_samples)
+    threshold_stimulus_level = threshold
+    width = 1 - sigmoid.alpha * 2
+
+    # Test that sigmoid(threshold_stimulus_level) == threshold_percent_correct
+    expected_PC = sigmoid.PC
+    if sigmoid.negative:
+        expected_PC = 1 - sigmoid.PC
+    np.testing.assert_allclose(sigmoid(threshold_stimulus_level, threshold, width), expected_PC)
+
+    # Test that the width is equal to the
+    # |X_L - X_R| == WIDTH, with
+    # with sigmoid(X_L) == ALPHA
+    # and  sigmoid(X_R) == 1 - ALPHA
+    prop_correct = sigmoid(stimulus_levels, threshold, width)
+    # When the sigmoid is negative, it is decreasing so we compute the width on 1-prop_correct
+    # (Alternatively, we could have used `argmax` and swapped the indices)
+    if sigmoid.negative:
+        prop_correct = 1 - prop_correct
+    idx_alpha, idx_nalpha = ((prop_correct < sigmoid.alpha).argmin(),
+                             (prop_correct < (1 - sigmoid.alpha)).argmin())
+    np.testing.assert_allclose(prop_correct[idx_nalpha] - prop_correct[idx_alpha], width,
+                               atol=0.02)
+
+    # Inverse sigmoid at threshold proportion correct (y-axis)
+    # Expects the threshold stimulus level (x-axis).
+    stimulus_level_from_inverse = sigmoid.inverse(expected_PC,
+                                                  threshold=threshold,
+                                                  width=width)
+    np.testing.assert_allclose(stimulus_level_from_inverse, threshold_stimulus_level)
+    # Expects inverse(value(x)) == x
+    y = sigmoid(stimulus_levels, threshold=threshold, width=width)
+    np.testing.assert_allclose(stimulus_levels,
+                               sigmoid.inverse(y, threshold=threshold, width=width),
+                               atol=1e-9)
+
+    slope = sigmoid.slope(stimulus_levels, threshold=threshold, width=width, gamma=0, lambd=0)
+    # Expects maximal slope at a medium stimulus level
+    #assert 0.4 * len(slope) < np.argmax(np.abs(slope)) < 0.6 * len(slope)
+    # Expects slope to be all positive/negative for standard/decreasing sigmoid
+    if sigmoid.negative:
+        assert np.all(slope < 0)
+    else:
+        assert np.all(slope > 0)
 
 
 def test_ALL_SIGMOID_NAMES():
