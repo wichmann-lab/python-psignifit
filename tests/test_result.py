360541cb4a471b0dfeae62caef844eb64da92af9
Tiziano Zito <opossumnano@gmail.com>
2025-01-06 11:26:58 +0100

    make test faster when we don't care about precision of fit

--- tests/test_result.py
+++ tests/test_result.py
@@ -298,7 +298,7 @@ def test_threshold_bug_172(input_data):
 
 
 def test_posterior_samples_raises_if_not_debug(input_data):
-    result = psignifit(input_data)
+    result = psignifit(input_data[:3,:])
     with pytest.raises(ValueError):
         result.posterior_samples(n_samples=10)
 
