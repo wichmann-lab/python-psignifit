7894815a7d13b2084ea8e48e120d98873b9e9f40
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2025-01-10 13:25:08 +0100

    docs: adds plot of psychometric function parameters and code to generate it

---
+++ docs/produce_sigmoid_plot_for_docs.py
@@ -0,0 +1,93 @@
+import numpy as np
+
+from psignifit import psignifit
+from psignifit import psigniplot
+from psignifit import sigmoids
+
+import matplotlib.pyplot as plt
+
+
+sigmoid = sigmoids.sigmoid_by_name('norm', PC=0.5, alpha=0.05)
+
+
+threshold = 0.5
+width = 0.5
+gamma = 0.1
+lambda_ = 0.1
+
+x = np.linspace(1e-12, 1-1e-12, num=10000)
+y = sigmoid(x, threshold, width, gamma, lambda_)
+
+
+fig, ax = plt.subplots(1)
+ax.plot(x, y, color='k', linewidth=3)
+ax.spines[['top', 'right']].set_visible(False)
+
+
+# threshold lines
+ax.vlines(x=threshold, ymin=0, ymax=0.5, color='k', linestyle='-')
+ax.hlines(y=0.5, xmin=x.min(), xmax=threshold, color='k', linestyle='-')
+
+# gamma and lambda dashed lines
+ax.hlines(y=(1-lambda_), xmin=x.min()-0.05, xmax=x.max(), color='k', linestyle='--')
+ax.hlines(y=gamma, xmin=x.min()-0.05, xmax=x.max(), color='k', linestyle='--')
+
+
+# width dashed lines
+x2 = sigmoid.inverse(0.95, threshold, width)
+x1 = sigmoid.inverse(0.05, threshold, width)
+
+ax.vlines(x=x1, ymin=0, ymax=sigmoid(x1, threshold, width, gamma, lambda_), color='k', linestyle='--')
+ax.vlines(x=x2, ymin=0, ymax=sigmoid(x2, threshold, width, gamma, lambda_), color='k', linestyle='--')
+
+
+# annotate threshold
+ax.annotate('threshold', xy=(0.5, -0.05), xytext=(0.5, -0.05), xycoords='axes fraction', textcoords='axes fraction',
+            ha='center', va='center', fontsize=10)
+
+# annotate width
+arrow_linecolor='black'
+arrow_linewidth=1
+
+ax.annotate('', xy=(x1, -0.12), xytext=(x2, -0.12), xycoords='axes fraction', textcoords='axes fraction',
+            arrowprops={'arrowstyle': '<|-|>', 'color':arrow_linecolor, 'linewidth':arrow_linewidth})
+
+ax.annotate('width', xy=(x2, -0.12), xytext=(x2, -0.12), xycoords='axes fraction', textcoords='axes fraction',
+            ha='left', va='center', fontsize=10)
+
+
+# annotate gamma
+yoffset = -0.06
+
+
+ax.annotate('', xy=(yoffset, -0.01), xytext=(yoffset, gamma+0.01), xycoords='axes fraction', textcoords='axes fraction',
+            arrowprops={'arrowstyle': '<|-|>', 'color':arrow_linecolor, 'linewidth':arrow_linewidth})
+
+ax.annotate(r'guess rate ($\gamma$)', xy=(yoffset-0.025, 0), xytext=(yoffset-0.025, gamma/2), 
+            xycoords='axes fraction', textcoords='axes fraction',
+            rotation=90,
+            ha='center', va='center', fontsize=10)
+
+# annotate lambda
+ax.annotate('', xy=(yoffset, 1-lambda_-0.01), xytext=(yoffset, 1+0.01), xycoords='axes fraction', textcoords='axes fraction',
+            arrowprops={'arrowstyle': '<|-|>', 'color':arrow_linecolor, 'linewidth':arrow_linewidth})
+
+
+ax.annotate(r'lapse rate ($\lambda$)', xy=(yoffset-0.025, 0), xytext=(yoffset-0.025, (1-lambda_/2)), 
+            xycoords='axes fraction', textcoords='axes fraction',
+            rotation=90,
+            ha='center', va='center', fontsize=10)
+
+
+ax.set_ylim(0, 1)
+ax.set_xlim(0, 1)
+ax.set_xticks([0, 1])
+ax.set_xticklabels(["0", "1"], fontsize=11)
+ax.set_yticks([0, 1])
+ax.set_yticklabels(["0", "1"], fontsize=11)
+ax.set_ylabel('Percent correct', fontsize=14)
+ax.set_xlabel('Stimulus level', fontsize=14)
+ax.xaxis.set_label_coords(.5, -.2)
+ax.yaxis.set_label_coords(-.15, .5)
+
+fig.savefig('sigmoid_plot_params.png', bbox_inches='tight')
