237135d03a5b587451a093c7aac0633f202f0863
Guillermo Aguilar <gmo.aguilar.c@gmail.com>
2025-06-15 15:26:17 +0200

    fixes bug in x-range of posterior samples plot

--- psignifit/psigniplot.py
+++ psignifit/psigniplot.py
@@ -669,7 +669,7 @@ def plot_posterior_samples(
 
     # Plot the samples from the posterior
     sigmoid = result.configuration.make_sigmoid()
-    x = np.linspace(0.001, 0.01, num=1000)
+    x = np.linspace(result.data[:, 0].min(), result.data[:, 0].max(), num=1000)
     for idx in range(n_samples):
         y = sigmoid(
             x,
